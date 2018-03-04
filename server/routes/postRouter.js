db3d2c822eab45c8d5d8dc57dbb06f1bf539fcf0
Liza Gulitski <liza.in.2009@gmail.com>
2018-03-04 18:46:30 +0200

    slash fix in post router

--- server/routes/postRouter.js
+++ server/routes/postRouter.js
@@ -5,7 +5,7 @@ const router = express.Router()
 
 router.get('/', postController.findAll)
 router.post('/', postController.create)
-router.get('id/:id', postController.withComments)
+router.get('/id/:id', postController.withComments)
 router.put('/:id', postController.update)
 router.delete('/:id', postController.delete)
 router.get('/stats', postController.stats)

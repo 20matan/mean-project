696c99f791f64f6880f117723cc68a9c36e5f184
DorWeid <dorprozor@gmail.com>
2018-03-07 15:05:47 +0200

    fixed hero population on post creation

--- server/controllers/postController.js
+++ server/controllers/postController.js
@@ -1,108 +1,134 @@
-const _ = require('lodash')
-const Post = require('../models/postModel')
+const _ = require("lodash");
+const Post = require("../models/postModel");
 
-const PostController = {}
+const PostController = {};
 
 PostController.withComments = (req, res, next) => {
-  const { id } = req.params
-  console.log('post find', id)
+  const { id } = req.params;
+  console.log("post find", id);
   Post.findById(id)
-    .populate('comments')
-    .populate('hero')
+    .populate("comments")
+    .populate("hero")
     .then(post => res.send({ success: true, post }))
-    .catch(next)
-}
+    .catch(next);
+};
 
-PostController.findByIdWithoutRes = id => Post.findById(id)
+PostController.findByIdWithoutRes = id => Post.findById(id);
 
 PostController.findById = (req, res, next) => {
-  const { id } = req.params
-  console.log('post find', id)
+  const { id } = req.params;
+  console.log("post find", id);
   Post.findById(id)
-    .populate('hero')
+    .populate("hero")
     .then(post => res.send({ success: true, post }))
-    .catch(next)
-}
+    .catch(next);
+};
 
 PostController.stats = (req, res, next) => {
   Post.aggregate([
-    { $group: { _id: '$hero', count: { $sum: 1 } } },
+    { $group: { _id: "$hero", count: { $sum: 1 } } },
     {
       $lookup: {
-        from: 'heros', localField: '_id', foreignField: '_id', as: 'hero',
-      },
+        from: "heros",
+        localField: "_id",
+        foreignField: "_id",
+        as: "hero"
+      }
     },
-    { $unwind: { path: '$hero' } }, // from [] to {}
+    { $unwind: { path: "$hero" } } // from [] to {}
   ])
-    .then((stats) => {
-      console.log('a')
-      res.send({ success: true, stats })
+    .then(stats => {
+      console.log("a");
+      res.send({ success: true, stats });
     })
-    .catch(next)
-}
+    .catch(next);
+};
 
 PostController.findAll = (req, res, next) => {
-  const { role, authorName, hero } = req.query
-  const findQuery = {}
-  const populationQuery = {}
+  const { role, authorName, hero } = req.query;
+  const findQuery = {};
+  const populationQuery = {};
 
   if (authorName) {
-    findQuery.authorName = authorName
+    findQuery.authorName = authorName;
   }
   if (hero) {
-    findQuery.hero = hero
+    findQuery.hero = hero;
   }
   if (role) {
-    populationQuery.role = role
+    populationQuery.role = role;
   }
 
   Post.find(findQuery)
     .populate({
-      path: 'hero',
-      match: populationQuery,
+      path: "hero",
+      match: populationQuery
     })
-    .then((findRes) => {
+    .then(findRes => {
       const posts = findRes
         .map(p => Object.assign({}, p.toObject(), { comments: undefined }))
-        .filter(p => p.hero)
+        .filter(p => p.hero);
 
       res.send({
         success: true,
-        posts,
-      })
+        posts
+      });
     })
-    .catch(next)
-}
+    .catch(next);
+};
 PostController.create = (req, res, next) => {
-  const postData = _.pick(req.body, ['title', 'authorName', 'authorSiteURL', 'date', 'content', 'imageURL', 'videoURL', 'hero'])
-  console.log('postData', postData, req.body)
+  const postData = _.pick(req.body, [
+    "title",
+    "authorName",
+    "authorSiteURL",
+    "date",
+    "content",
+    "imageURL",
+    "videoURL",
+    "hero"
+  ]);
+  console.log("postData", postData, req.body);
 
-  const newPost = Post(postData)
-  newPost.save()
-    .then(post => res.send({ success: true, post }))
-    .catch(next)
-}
+  const newPost = Post(postData);
+  newPost
+    .save()
+    .then(post =>
+      Post.findById(post._id)
+        .populate("comments")
+        .populate("hero")
+        .then(populated => res.send({ success: true, post: populated }))
+    )
+    .catch(next);
+};
 PostController.update = (req, res, next) => {
-  console.log('will update ', req.params.id, 'with data', req.body)
-  Post.findByIdAndUpdate(req.params.id, req.body, { runValidators: true, new: true })
+  console.log("will update ", req.params.id, "with data", req.body);
+  Post.findByIdAndUpdate(req.params.id, req.body, {
+    runValidators: true,
+    new: true
+  })
     .then(post => res.send({ success: true, post }))
-    .catch(next)
-}
+    .catch(next);
+};
 PostController.delete = (req, res, next) => {
   Post.findByIdAndRemove(req.params.id)
     .then(() => res.send({ succes: true }))
-    .catch(next)
-}
+    .catch(next);
+};
 
 PostController.getMore = (req, res, next) => {
-  const { postId, heroId } = req.params
+  const { postId, heroId } = req.params;
   Post.find({})
-    .then(posts => res.send({
-      success: true,
-      posts: posts
-        .filter(p => p.toObject().postId !== postId && p.toObject().heroId !== heroId)
-        .map(p => Object.assign({}, p.toObject(), { comments: undefined })),
-    }))
-    .catch(next)
-}
-module.exports = PostController
+    .then(posts =>
+      res.send({
+        success: true,
+        posts: posts
+          .filter(
+            p =>
+              p.toObject().postId !== postId && p.toObject().heroId !== heroId
+          )
+          .map(p => Object.assign({}, p.toObject(), { comments: undefined }))
+      })
+    )
+    .catch(next);
+};
+module.exports = PostController;
